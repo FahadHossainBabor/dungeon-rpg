c761a37f5620e243016424f90735fbed1813333c
FahadHossainBabor <fahadhossainbabor.cse.ruet@gmail.com>
2026-09-04 19:15:48 +0600

    Implement enemy damage handling

--- enemy.py
+++ enemy.py
@@ -2,3 +2,6 @@ class Enemy:
     def __init__(self,name,health):
         self.name = name
         self.health = health
+    
+    def take_damage(self, damage):
+        self.health = max(0, self.health - damage)
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from enemy import Enemy
 
 
 player = Player("Fahad", 100)
+enemy = Enemy("Goblin", 50)
+
+damage = player.attack()
+enemy.take_damage(damage)
+
 print(player.name)
 print(player.health)
 
-damage = player.attack()
-print("Attack damage:", damage)
-
-enemy = Enemy("Goblin",50)
 print(enemy.name)
 print(enemy.health)
