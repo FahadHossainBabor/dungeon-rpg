96c7d0a3892d4198fab658be3b62d70c3da2a32d
FahadHossainBabor <fahadhossainbabor.cse.ruet@gmail.com>
2026-09-04 19:05:03 +0600

    Create Enemy class

---
+++ enemy.py
@@ -0,0 +1,4 @@
+class Enemy:
+    def __init__(self,name,health):
+        self.name = name
+        self.health = health
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from player import Player
+from enemy import Enemy
+
 
 player = Player("Fahad", 100)
-
 print(player.name)
-
 print(player.health)
 
 damage = player.attack()
-
 print("Attack damage:", damage)
+
+enemy = Enemy("Goblin",50)
+print(enemy.name)
+print(enemy.health)
